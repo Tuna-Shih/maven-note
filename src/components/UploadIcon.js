d100f0c8b83f37d3c2be588eff848052737224ee
Tuna-Shih <61424827+Tuna-Shih@users.noreply.github.com>
2020-09-03 21:20:13 +0800

    feat: upload  api

--- src/components/UploadIcon.js
+++ src/components/UploadIcon.js
@@ -60,7 +60,7 @@ const UploadIcon = forwardRef(({ value, onChange }, ref) => {
           beforeUpload={beforeUpload}
           onChange={handleChange}
           showUploadList={false}
-          action="http://localhost:3000/api/upload"
+          action="/api/upload"
           name="icon"
           listType="picture-card"
         >
