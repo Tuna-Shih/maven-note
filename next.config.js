dbbce1058ab491e4c73a3d1473b86898c560b3f7
Tuna-Shih <61424827+Tuna-Shih@users.noreply.github.com>
2020-09-03 15:17:25 +0800

    feat: next

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@
 const withLess = require('@zeit/next-less');
 const emptyFunction = require('fbjs/lib/emptyFunction');
 
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
 /* ::
 type ruleType = {
@@ -29,10 +29,10 @@ type configType = {
 if (typeof require !== 'undefined') require.extensions['.less'] = emptyFunction;
 
 module.exports = withLess({
-  exportPathMap: () => ({
-    '/': { page: '/' },
-  }),
-  assetPrefix: isProd ? './' : '',
+  // exportPathMap: () => ({
+  //   '/': { page: '/' },
+  // }),
+  // assetPrefix: isProd ? './' : '',
   cssModules: true,
   lessLoaderOptions: {
     modifyVars: {
